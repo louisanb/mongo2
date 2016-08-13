4f74496596eca86b657e7b60143503aef299206c
louis qiyu an <louis qiyu an>
2016-08-13 11:47:46 -0700

    sec

---
+++ fe/src/app/main/main.controller.js
@@ -0,0 +1,12 @@
+export class MainController {
+  constructor ($http) {
+    'ngInject';
+
+  this.$http=$http;
+  }
+
+    postMessage(){
+      // console.log("post");
+      this.$http.post('http"//localhost:5000/api/message', {msg: 'hello'});
+    }
+  }
